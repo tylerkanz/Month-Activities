f74421007145c85e100d0dda7dc6e2c266de192f
tylerkanz <tylerkanz@gmail.com>
2020-05-18 12:36:20 -0500

    first commit

---
+++ Month2/Month2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Month2
+{
+    public partial class Form1 : Form
+    {
+        int monthNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            monthNum = int.Parse(textBox1.Text);
+
+            switch (monthNum)
+            {
+                case 1:
+                    label2.Text = "The month is January!";
+                    break;
+                case 2:
+                    label2.Text = "The month is Feburary!";
+                    break;
+                case 3:
+                    label2.Text = "The month is March!";
+                    break;
+                case 4:
+                    label2.Text = "The month is April!";
+                    break;
+                case 5:
+                    label2.Text = "The month is May!";
+                    break;
+                case 6:
+                    label2.Text = "The month is June!";
+                    break;
+                case 7:
+                    label2.Text = "The month is July!";
+                    break;
+                case 8:
+                    label2.Text = "The month is August!";
+                    break;
+                case 9:
+                    label2.Text = "The month is September!";
+                    break;
+                case 10:
+                    label2.Text = "The month is October!";
+                    break;
+                case 11:
+                    label2.Text = "The month is November!";
+                    break;
+                case 12:
+                    label2.Text = "The month is December!";
+                    break;
+                default:
+                    label2.Text = "Incorrect Format!";
+                    break;
+            }
+
+            label2.Visible = true;
+
+        }
+    }
+}
---
+++ MonthA/MonthA/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthA
+{
+    public partial class Form1 : Form
+    {
+        string month;
+        int monthnum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            monthnum = int.Parse(textBox1.Text);
+            
+
+            if (monthnum == 1)
+            {
+                month = "The month is January!";
+            }
+
+            else if (monthnum == 2)
+            {
+                month = "The month is Feburary!";
+            }
+
+            else if (monthnum == 3)
+            {
+                month = "The month is March!";                
+            }
+
+            else if (monthnum == 4)
+            {
+                month = "The month is April!";
+            }
+
+            else if (monthnum == 5)
+            {
+                month = "The month is May!";
+            }
+
+            else if (monthnum == 6)
+            {
+                month = "The month is June!";
+            }
+
+            else if (monthnum == 7)
+            {
+                month = "The month is July!";
+            }
+
+            else if (monthnum == 8)
+            {
+                month = "The month is August!";
+            }
+
+            else if (monthnum == 9)
+            {
+                month = "The month is September!";
+            }
+
+            else if (monthnum == 10)
+            {
+                month = "The month is October!";
+            }
+
+            else if (monthnum == 11)
+            {
+                month = "The month is November!";
+            }
+
+            else if (monthnum == 12)
+            {
+                month = "The month is December!";
+            }
+
+            else
+            {
+                month = "Incorrect Format"; 
+            }
+
+
+            //Label Creation
+            label2.Text = month;
+            label2.Visible = true;
+
+
+        }
+    }
+}
